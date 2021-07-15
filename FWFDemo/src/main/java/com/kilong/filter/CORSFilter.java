71205a9c6fa72822f17f805fc039e73f6464ed9b
PANGLING998 <Zzrt4396@.com>
2021-07-15 17:14:13 +0800

    solve cors

--- FWFDemo/src/main/java/com/kilong/filter/CORSFilter.java
+++ FWFDemo/src/main/java/com/kilong/filter/CORSFilter.java
@@ -17,11 +17,14 @@ public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         HttpServletResponse response1= (HttpServletResponse) response;
-//        response1.addHeader("Access-Control-Allow-Origin","*");
-//        response1.setHeader("Access-Control-Allow-Origin", "*");
-//        response1.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-//        response1.setHeader("Access-Control-Max-Age", "3600");
-//        response1.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        chain.doFilter(request, response);
+        HttpServletRequest request1= (HttpServletRequest) request;
+        response.setContentType("text/html;charset=UTF-8");
+        response1.setHeader("Access-Control-Allow-Origin", "*");
+        response1.setHeader("Access-Control-Allow-Methods", "*");
+        response1.setHeader("Access-Control-Max-Age", "3600");
+        response1.setHeader("Access-Control-Allow-Headers", "Origin,No-Cache,x-requested-with,If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,Authorization,SessionToken,JSESSIONID,token");
+        response1.setHeader("Access-Control-Allow-Credentials", "true");
+        response1.setHeader("XDomainRequestAllowed","1");
+        chain.doFilter(request1, response1);
     }
 }
